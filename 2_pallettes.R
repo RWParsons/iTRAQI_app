f7909525da9ed6c66bf66d0d5355f877d148ee1d
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2022-04-07 14:39:47 +1000

    Update 2_pallettes.R

--- 2_pallettes.R
+++ 2_pallettes.R
@@ -1,26 +1,29 @@
-bins <- c(0, 30, 60, 120, 180, 240, 300, 360, 900)
-palBin <- colorBin("YlOrRd", domain = 0:900, bins=bins, na.color="transparent")
+bins <- c(0, 30, 60, 120, 180, 240, 300, 360, 900, 1200)
 
-palNum1 <- colorNumeric(c("#FFFFCC", "#FFEDA0"), domain=0:30, na.color="transparent")
-palNum2 <- colorNumeric(c("#FFEDA0", "#FED976"), domain=30:60, na.color="transparent")
-palNum3 <- colorNumeric(c("#FED976", "#FEB24C"), domain=60:120, na.color="transparent")
-palNum4 <- colorNumeric(c("#FEB24C", "#FD8D3C"), domain=120:180, na.color="transparent")
-palNum5 <- colorNumeric(c("#FD8D3C", "#FC4E2A"), domain=180:240, na.color="transparent")
-palNum6 <- colorNumeric(c("#FC4E2A", "#E31A1C"), domain=240:300, na.color="transparent")
-palNum7 <- colorNumeric(c("#E31A1C", "#B10026"), domain=300:360, na.color="transparent")
-palNum8 <- colorNumeric(c("#B10026", "#000000"), domain=360:900, na.color="transparent")
+palBin <- colorBin("YlOrRd", domain = min(bins):max(bins), bins=bins, na.color="transparent")
+
+palNum1 <- colorNumeric(c(palBin(bins[1]), palBin(bins[2])), domain=0:30, na.color="transparent")
+palNum2 <- colorNumeric(c(palBin(bins[2]), palBin(bins[3])), domain=30:60, na.color="transparent")
+palNum3 <- colorNumeric(c(palBin(bins[3]), palBin(bins[4])), domain=60:120, na.color="transparent")
+palNum4 <- colorNumeric(c(palBin(bins[4]), palBin(bins[5])), domain=120:180, na.color="transparent")
+palNum5 <- colorNumeric(c(palBin(bins[5]), palBin(bins[6])), domain=180:240, na.color="transparent")
+palNum6 <- colorNumeric(c(palBin(bins[6]), palBin(bins[7])), domain=240:300, na.color="transparent")
+palNum7 <- colorNumeric(c(palBin(bins[7]), palBin(bins[8])), domain=300:360, na.color="transparent")
+palNum8 <- colorNumeric(c(palBin(bins[8]), palBin(bins[9])), domain=360:900, na.color="transparent")
+palNum9 <- colorNumeric(c(palBin(bins[9]), "#000000"), domain=900:1200, na.color="transparent")
 
 palNum <- function(x){
   case_when(
-    x < 30 ~ palNum1(x),
-    x < 60 ~ palNum2(x),
+    x < 30  ~ palNum1(x),
+    x < 60  ~ palNum2(x),
     x < 120 ~ palNum3(x),
     x < 180 ~ palNum4(x),
     x < 240 ~ palNum5(x),
     x < 300 ~ palNum6(x),
     x < 360 ~ palNum7(x),
     x < 900 ~ palNum8(x),
-    x >= 900 ~ "#000000",
+    x <1200 ~ palNum9(x),
+    x >=1200~ "#000000",
     TRUE ~ "transparent"
   )
 }
