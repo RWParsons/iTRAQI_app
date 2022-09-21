1c326fc38b0c1ae9e594a6612597edc950d88f42
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2022-09-21 13:51:04 +1000

    remove factoid

--- 5_loading_displays.R
+++ 5_loading_displays.R
@@ -3,11 +3,6 @@ loader_img_width <- 500
 loading_panel_displays <- c(
   paste(
     sep = seperator,
-    "<h2>Humans are the Only Animals That Enjoy Spicy Foods (factoid)</h2>",
-    glue::glue('<img src="tour-1-tbi-image.jpg" alt="tbi-image" style="width:{loader_img_width}px;">')
-  ),
-  paste(
-    sep = seperator,
     "<h2>There were 1,024,242 Triple Zero calls in the 2020-21 FY</h2>",
     glue::glue('<img src="tour-3-ambulance.png" alt="ambulance-image" style="width:{loader_img_width}px;">')
   ),
